c3d2e7c40057645aa8f9dff7786f75467cbda18d
Janrik <janrik@codebrew.fi>
2024-08-31 15:21:57 +0300

    linear track
    stable in gazebo
    
    nope..not stable
    
    testing
    
    promising
    
    yes! next control

--- ar_gazebo/launch/ar_gazebo.launch.py
+++ ar_gazebo/launch/ar_gazebo.launch.py
@@ -100,6 +100,7 @@ def generate_launch_description():
         ],
     )
 
+
     # Gazebo nodes
     gazebo = IncludeLaunchDescription(
         PythonLaunchDescriptionSource(
--- helpers/publish_state.sh
+++ helpers/publish_state.sh
@@ -1,1 +1,3 @@
-ros2 run robot_state_publisher robot_state_publisher --ros-args -p robot_description:="$( xacro ~/ar4_ros_driver/ar_description/urdf/ar.urdf.xacro ar_model:=ar4_mk3)"
+screen ros2 run robot_state_publisher robot_state_publisher --ros-args -p robot_description:="$( xacro ~/ar4_ros_driver/ar_description/urdf/ar.urdf.xacro ar_model:=ar4_mk3)"
+screen ros2 run joint_state_publisher_gui joint_state_publisher_gui
+
